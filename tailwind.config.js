c2115b4caeb30d1c92af1fa7e204a2fc46ae6f44
Paolo Delfino <paolodelfino9@gmail.com>
2022-11-26 09:56:37 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/scripts/*.js", "./public/stylesheets/css/*.css", "./public/stylesheets/sass/*.css", "./views/pages/*.ejs", "./views/partials/*.ejs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
